f46d110012fb573fe5e3e17b2b4bad0312208186
Kevin Engelmann <kevin.engelmann@parx.com>
2018-01-09 14:11:06 +0100

    Adding new commands and functionalities

--- extension.js
+++ extension.js
@@ -1,71 +1,176 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+var spawnCMD = require('spawn-command');
+var treeKill = require('tree-kill');
+var process = null;
+var commandOutput = null;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function run(cmd, cwd) {
+    return new Promise((accept, reject) => {
+      var opts = {};
+      if (vscode.workspace) {
+        opts.cwd = cwd;
+      }
+      process = spawnCMD(cmd, opts);
+      function printOutput(data) { commandOutput.append(data.toString()); }
+      process.stdout.on('data', printOutput);
+      process.stderr.on('data', printOutput);
+      process.on('close', (status) => {
+        if (status) {
+          reject(`Command \`${cmd}\` exited with status code ${status}.`);
+        } else {
+          accept();
+        }
+        process = null;
+      });
+    });
+}
+
+function exec(cmd, cwd) {
+    if (!cmd) { return; }
+    commandOutput.clear();
+    commandOutput.show();
+    commandOutput.appendLine(`> Running command \`${cmd}\`...`)
+    run(cmd, cwd).then(() => {
+      commandOutput.appendLine(`> Command ran successfully.`);
+    }).catch((reason) => {
+      commandOutput.appendLine(`> ERROR: ${reason}`);
+      vscode.window.showErrorMessage(reason, 'Show Output')
+        .then((action) => { commandOutput.show(); });
+    });
+}
+
+function term() {
+    treeKill(process.pid, 'SIGTERM', function(err) {
+      if (err) {
+        vscode.window.showErrorMessage(`Failed to kill process with PID ${process.pid}.`);
+      } else {
+        process = null;
+      }
+    });
+}
+
+function execShellCMD(cwd, cmd) {
+    if (process) {
+        const msg = 'There is an active running shell command right now. Terminate it before executing another shell command.';
+        vscode.window.showWarningMessage(msg, 'Terminate')
+        .then((choice) => {
+            if (choice === 'Terminate') {
+            term();
+            }
+        });
+    } else {
+        exec(cmd, cwd);
+    }
+}
 function activate(context) {
-    var term = null;
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "force-dev-tool-for-vs" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    context.subscriptions.push(vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World to force!');
-    }));
+    commandOutput = vscode.window.createOutputChannel('force-dev-tool');
     
     context.subscriptions.push(vscode.commands.registerCommand('extension.login', function () {
-        if(term == null){
-            term = vscode.window.createTerminal(`Force-dev-tool Terminal`);
-        } 
-        /*
-        vscode.window.showQuickPick(['option 1', 'option 2', 'option 3'])
-        .then(val => {
-            vscode.window.showInformationMessage('You picked ' + val)
-            term.show();
-            term.sendText("force-dev-tool login");
-        });*/
+        execShellCMD(vscode.workspace.rootPath, "force-dev-tool login");
     }));
     context.subscriptions.push(vscode.commands.registerCommand('extension.fetch', function () {
-        if(term == null){
-            term = vscode.window.createTerminal(`Force-dev-tool Terminal`);
-        } 
-        term.show();
-        term.sendText("force-dev-tool fetch");
+        execShellCMD(vscode.workspace.rootPath, "force-dev-tool fetch");        
     }));
     context.subscriptions.push(vscode.commands.registerCommand('extension.retrieve', function () {
-        if(term == null){
-            term = vscode.window.createTerminal(`Force-dev-tool Terminal`);
-        } 
-        term.show();
-        term.sendText("force-dev-tool retrieve");
+        execShellCMD(vscode.workspace.rootPath, "force-dev-tool retrieve");                
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.test', function () {
+        execShellCMD(vscode.workspace.rootPath, "force-dev-tool test");                
     }));
     context.subscriptions.push(vscode.commands.registerCommand('extension.packageAll', function () {
-        if(term == null){
-            term = vscode.window.createTerminal(`Force-dev-tool Terminal`);
-        } 
-        term.show();
-        term.sendText("force-dev-tool package -a");
+        execShellCMD(vscode.workspace.rootPath, "force-dev-tool package -a");                
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.destructive', function (file) {
+        var options = {
+            placeHolder: 'Changeset Name',
+        };
+        vscode.window.showInputBox(options).then((name) => {
+            commandOutput.show();
+            var path; 
+            if(file == null)
+            {
+                var options2 = {
+                    placeHolder: 'Filename',
+                };
+                vscode.window.showInputBox(options2).then((file) => {
+                    execShellCMD(vscode.workspace.rootPath, 'echo "" | force-dev-tool changeset create ' + name + ' --destructive ' + file );                                    
+                });
+            }else{
+                path = file.toString();
+                path = path.replace("file://", "");
+                execShellCMD(vscode.workspace.rootPath, 'echo "" | force-dev-tool changeset create ' + name + ' --destructive ' + path);                                
+            }
+            
+        });
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.changeset', function (file) {
+        
+        var options = {
+            placeHolder: 'Changeset Name.',
+        };
+        vscode.window.showInputBox(options).then((name) => {
+            commandOutput.show();
+
+            var path; 
+            if(file == null)
+            {
+                var options2 = {
+                    placeHolder: 'Filename',
+                };
+                vscode.window.showInputBox(options2).then((file) => {
+                    execShellCMD(vscode.workspace.rootPath, 'echo "" | force-dev-tool changeset create ' + name + ' -f ' + file);                                    
+                });
+            }else{
+                path = file.toString();
+                path = path.replace("file://", "");
+                execShellCMD(vscode.workspace.rootPath, 'echo "" | force-dev-tool changeset create ' + name + ' -f ' + path);                
+            }
+        });
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.createDiffChangeset', function () {
+        var options = {
+            placeHolder: 'Changeset Name.',
+        };
+        vscode.window.showInputBox(options).then((name) => {
+            execShellCMD(vscode.workspace.rootPath, "git diff | force-dev-tool changeset create -f " + name);                
+        });
     }));
     context.subscriptions.push(vscode.commands.registerCommand('extension.check', function () {
-        if(term == null){
-            term = vscode.window.createTerminal(`Force-dev-tool Terminal`);
-        } 
-        term.show();
-        term.sendText("Checking your changes since the last commit agains your org. ");
-        term.sendText("git diff | force-dev-tool changeset create foo2");
-        term.sendText("force-dev-tool deploy -d ./config/deployments/foo2 -c");        
-        term.sendText("rm -R ./config/deployments/foo2");
+        execShellCMD(vscode.workspace.rootPath, "git diff | force-dev-tool changeset create tempChangeSet -f ; force-dev-tool deploy -d config/deployments/tempChangeSet/ -c");                
     }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.deploy', function (file) {
+            commandOutput.show();
+
+            var path; 
+            if(file == null)
+            {
+                var options2 = {
+                    placeHolder: 'Filename',
+                };
+                vscode.window.showInputBox(options2).then((file) => {
+                    execShellCMD(vscode.workspace.rootPath, 'echo "" | force-dev-tool changeset create tempChangeSet -f ' + file +' && force-dev-tool deploy -d config/deployments/tempChangeSet/' );                                                         
+                });
+            }else{
+                path = file.toString();
+                path = path.replace("file://", "");
+                execShellCMD(vscode.workspace.rootPath, 'echo "" | force-dev-tool changeset create tempChangeSet -f ' + path +' && force-dev-tool deploy -d config/deployments/tempChangeSet/' );                                                         
+            }
+        }));    
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {
+    if (process) {
+        treeKill(process.pid, 'SIGTERM', function(err) {
+          if (err) {
+            treeKill(process.pid, 'SIGKILL');
+          }
+          process = null;
+        });
+    }
 }
 exports.deactivate = deactivate;
